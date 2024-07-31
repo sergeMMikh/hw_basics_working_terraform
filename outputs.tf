65cdcdcc11d479baab26026326a41a7ad8a92358
smmih <s.m.mih@mail.ru>
2024-07-31 23:17:20 +0100

    added vpc with cidrblock 10.0.0.0

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+# output "srv_public_ip" {
+#   value = aws_instance.platform[0].public_ip
+# }
+
+# output "srv_public_dns" {
+#   value = aws_instance.platform[0].public_dns
+# }
+
+# output "srv_private_ip" {
+#   value = aws_instance.platform[0].private_ip
+# }
+
+output "srv_public_ip" {
+  value = aws_instance.platform[0].public_ip
+}
+
+output "srv_public_dns" {
+  value = aws_instance.platform[0].public_dns
+}
+
+output "srv_private_ip" {
+  value = aws_instance.platform[0].private_ip
+}
+
+output "subnet_cidr_block" {
+  value = aws_subnet.develop.cidr_block
+}
+
+output "instance_private_ip_in_subnet" {
+  value = aws_instance.platform[0].private_ip
+}
